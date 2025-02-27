1103b44a3fb94de07576638f3d4cfd2b8cd5e3b9
Nikoloz-Lortkipanidze <nikusha191208@gmail.com>
2025-02-27 13:41:30 +0400

    functions!

---
+++ lesson3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void startprogam(){
+  printf("program started.\n");
+}
+
+void endprogram(void){
+  printf("program ended.\n");
+}
+
+void sayHello(char *name);
+
+int add(int, int);
+
+
+int main(){
+
+  startprogam("testing if i can pass parameter:)");
+
+  sayHello("Nikoloz"); 
+  
+  int a = 50;
+  int b = 75;
+  printf("%d + %d = %d", a, b, add(a,b));
+
+  endprogram();
+}
+
+
+void sayHello(char *name){
+  printf("Hello %s\n", name);
+}
+
+int add(int a, int b){
+  return a + b;
+}
