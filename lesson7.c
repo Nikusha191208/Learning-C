89cf1fdcb3b579bd197ea0692a8f5c785234312f
Nikusha191208 <nikusha191208@gmail.com>
2025-03-02 12:21:37 +0400

    structs <3

---
+++ lesson7.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+struct Human {
+   char *name;
+   int age;
+   float balance;
+};
+
+void printHuman(struct Human*);
+
+
+int main(){
+
+   struct Human human1 = {.name = "Nikoloz", .age = 16, .balance = 355.4 };
+
+   struct Human human2 = human1;
+   human2.name = "Dimitri";
+   human2.age = 14;
+  
+   printHuman(&human1);
+   printHuman(&human2);
+
+}
+
+
+void printHuman(struct Human *human){
+   printf("Name: %s, Age: %d, Balance: %.1f\n", human->name, human->age, human->balance);
+}
